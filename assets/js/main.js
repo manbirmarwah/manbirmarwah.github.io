65c8f95960972c868b41741e7feb0013cdff9bc5
Manbir Singh <manbirmarwah@gmail.com>
2021-06-15 01:15:08 +0530

    Updated design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -28,7 +22,7 @@
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
+			}, 500);
 		});
 
 	// Fix: Flexbox min-height bug on IE.
